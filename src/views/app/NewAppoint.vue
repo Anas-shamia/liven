6908096582e4e630d13b159fbcd5c1072c47d2ff
asalout <a.salout@dce.sa>
2020-04-03 04:47:10 +0300

    appointments

--- src/views/app/NewAppoint.vue
+++ src/views/app/NewAppoint.vue
@@ -3,40 +3,31 @@
         <Bar :title="'المواعيد'" :close="'/appointments'"/>
         <div class="mobile-padding py-4 min-h-screen bg-gray-100">
             <h2 class="text-2xl font-medium text-blue-800 mb-8 text-center">موعد جديد</h2>
-            <ValidationObserver ref="addAppoint">
+            <ValidationObserver ref="addAppointment">
                 <form @submit.prevent="handleSubmit" class="mt-4">
-
-                    <!--                    <ValidationProvider class="flex items-center flex-wrap mb-4" tag="div"-->
-                    <!--                                        vid="title" name="address" rules="required"-->
-                    <!--                                        v-slot="{ errors }">-->
-                    <div class="flex items-center flex-wrap mb-4">
-                        <label class="w-1/4 text-base text-blue-800 rtl:pl-8 ltr:pr-8">التاريخ</label>
-                        <div class="w-3/4 bg-white-900 rounded-25px py-3 px-6 focus:outline-none">
-                            <v-date-picker
-                                    v-model='form.date'
-                                    :dir="dirLang"
-                                    :popover="popover"
-                                    :available-dates="dates.days"
-                                    :min-date="dates.minDate"
-                                    :input-props='{
-                          class: "border-none focus:outline-none",
-                          placeholder: "تاريخ الموعد"
-                        }'
-                            />
-                        </div>
-                    </div>
+                    <ValidationProvider class="flex items-center flex-wrap mb-4" tag="div"
+                                        vid="appointment_id" name="appointment_id" rules="required"
+                                        v-slot="{ errors }">
+                        <label class="w-1/4 text-base text-blue-800 rtl:pl-8 ltr:pr-8">المواعيد</label>
+                        <v-select class="w-3/4 bg-white-900 rounded-25px py-3 px-6 appoint-select"
+                                  :options="options"
+                                  label="name"
+                                  placeholder="المواعيد" :dir="dirLang" v-model="form.appointment_id"
+                                  :class="{ 'has-danger': errors.length }">
+                        </v-select>
+                        <p class="message-danger">{{ errors[0] }}</p>
+                    </ValidationProvider>
 
                     <ValidationProvider class="flex items-center flex-wrap mb-4" tag="div"
-                                        vid="timing" name="timing" rules="required"
-                                        v-slot="{ errors, touched }">
-                        <label class="w-1/4 text-base text-blue-800 rtl:pl-8 ltr:pr-8">الوقت</label>
-                        <datetime type="time"
-                                  class="theme-purple w-3/4 bg-white-900 rounded-25px py-3 px-6 focus:outline-none"
-                                  :class="{ 'has-danger': (errors.length && touched) }"
-                                  v-model="form.timing"
-                                  placeholder="وقت الدواء" use12-hour></datetime>
-                        <p class="message-danger" v-if="touched">{{ errors[0] }}</p>
+                                        vid="title" name="title" rules="required"
+                                        v-slot="{ errors }">
+                        <label class="w-1/4 text-base text-blue-800 rtl:pl-8 ltr:pr-8">العنوان</label>
+                        <input type="text" class="w-3/4 bg-white-900 rounded-25px py-3 px-6 focus:outline-none"
+                               placeholder="العنوان" v-model="form.title"
+                               :class="{ 'has-danger': errors.length }">
+                        <p class="message-danger">{{ errors[0] }}</p>
                     </ValidationProvider>
+
                     <ValidationProvider class="flex items-center flex-wrap mb-4" tag="div" vid="notes"
                                         name="notes"
                                         rules="required"
@@ -53,6 +44,9 @@
                         <button type="submit"
                                 class="w-full text-white-900 text-base font-medium bg-blue-800 rounded-25px py-3">ارسال
                         </button>
+                    </div>
+                    <div class="bg-green-100 mt-4 rounded-10px text-center" v-if="success">
+                        <p class="p-3 text-base text-blue-800 font-medium">تمت الاضافة بنجاح</p>
                     </div>
                 </form>
             </ValidationObserver>
@@ -71,27 +65,25 @@
                 popover: {
                     visibility: 'focus',
                 },
-                options: [
-                    'anas',
-                    'Akmal'
-                ],
+                options: [],
                 theDay: null,
                 dates: {
                     minDate: new Date(this.getToday()),
                     days: [1, 4, 7]
                 },
                 form: {
+                    appointment_id: null,
                     title: null,
-                    note: null,
-                    date: null
-                }
+                    notes: null,
+                },
+                success: false,
+                loading: false,
             }
         },
         computed: {
             dirLang() {
                 return this.$i18n.locale === 'en' ? 'ltr' : 'rtl';
             },
-
         },
         methods: {
             getToday() {
@@ -101,10 +93,61 @@
                 let yyyy = today.getFullYear();
                 today = yyyy + '-' + mm + '-' + dd;
                 return this.theDay = today;
-            }
+            },
+            getAppointments() {
+                this.axios.get(`/mobile/appointment`)
+                    .then(res => {
+                        const $days = {
+                            0: 'الأحد',
+                            1: 'الإثنين',
+                            2: 'الثلاثاء',
+                            3: 'الأربعاء',
+                            4: 'الخميس',
+                            5: 'الجمعة',
+                            6: 'السبت',
+                        };
+                        this.options = res.data.data.map(x=> {
+                            return {
+                                id: x.id,
+                                name: `${$days[x.day]} من ${x.from} الى ${x.to}`
+                            }
+                        });
+                    })
+            },
+            handleSubmit() {
+                const $this = this;
+                this.$refs['addAppointment'].validate().then((result) => {
+                    if (result) {
+                        this.loading = true;
+                        let form = _.cloneDeep(this.form);
+                        form.appointment_id = form.appointment_id.id;
+                        this.axios.post('/mobile/appointment', form).then((res) => {
+                            this.success = true;
+                            this.loading = false;
+                            this.form = {
+                                appointment_id: null,
+                                title: null,
+                                note: null,
+                            };
+                            setTimeout(function () {
+                                $this.success = false;
+                            }, 3000);
+                            this.$refs['addAppointment'].reset();
+                        }).catch((error) => {
+                            this.loading = false;
+                            if (error.response) {
+                                if (error.response.status === 422) {
+                                    this.$refs['addAppointment'].setErrors(error.response.data.errors);
+                                }
+                            }
+                        });
+                    }
+                });
+            },
         },
         created() {
             this.getToday();
+            this.getAppointments();
         }
     }
 </script>
