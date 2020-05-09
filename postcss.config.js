fdde3c827810f7337cff00b15f399b8429289920
Anas shamia <a.shamia@dce.sa>
2020-05-09 23:23:46 +0300

    update

--- postcss.config.js
+++ postcss.config.js
@@ -5,18 +5,18 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        // cssnano({
-        //     preset: 'default'
-        // }),
-        // purgecss({
-        //     content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.jsx',
-        //         './node_modules/v-calendar/src/**/*.vue',
-        //         './node_modules/vue-datetime/src/*.vue',
-        //         './node_modules/vue-select/src/**/*.vue',
-        //     ],
-        //     // content: ['index.html', '**/*.js', '**/*.html', '**/*.vue'],
-        //     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        //     // (/[A-z0-9-:\\/%-.]+/g) || []
-        // }),
+        cssnano({
+            preset: 'default'
+        }),
+        purgecss({
+            content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.jsx',
+                './node_modules/v-calendar/src/**/*.vue',
+                './node_modules/vue-datetime/src/*.vue',
+                './node_modules/vue-select/src/**/*.vue',
+            ],
+            // content: ['index.html', '**/*.js', '**/*.html', '**/*.vue'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+            // (/[A-z0-9-:\\/%-.]+/g) || []
+        }),
     ]
 };
--- vue.config.js
+++ vue.config.js
@@ -3,25 +3,25 @@ const zlib = require('zlib');
 
 module.exports = {
     configureWebpack: {
-        // plugins: [
-        //     new CompressionPlugin({
-        //         filename: '[path].gz[query]',
-        //         algorithm: 'gzip',
-        //         test: /\.js$|\.css$|\.html$/,
-        //         threshold: 10240,
-        //         minRatio: 0.8,
-        //     }),
-        //     new CompressionPlugin({
-        //         filename: '[path].br[query]',
-        //         algorithm: 'brotliCompress',
-        //         test: /\.(js|css|html|svg)$/,
-        //         compressionOptions: {
-        //             level: 11,
-        //         },
-        //         threshold: 10240,
-        //         minRatio: 0.8,
-        //     }),
-        // ]
+        plugins: [
+            new CompressionPlugin({
+                filename: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: /\.js$|\.css$|\.html$/,
+                threshold: 10240,
+                minRatio: 0.8,
+            }),
+            new CompressionPlugin({
+                filename: '[path].br[query]',
+                algorithm: 'brotliCompress',
+                test: /\.(js|css|html|svg)$/,
+                compressionOptions: {
+                    level: 11,
+                },
+                threshold: 10240,
+                minRatio: 0.8,
+            }),
+        ]
     },
 
     pluginOptions: {
